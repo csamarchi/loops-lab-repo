a75b0fe1f6b90860e68b9edcdcb963b010f5a589
Christine Samarchi <christinesamarchi@Christines-MacBook-Air.local>
2018-09-11 15:06:04 -0500

    this is a test

---
+++ javascript/app.js
@@ -0,0 +1,55 @@
+// console.log("Making your way in the world today takes everything you've got.");
+// console.log("Taking a break from all your worries, sure would help a lot.");
+// console.log("Wouldn't you like to get away?");
+// console.log("Sometimes you want to go");
+// console.log("Where everybody knows your name,");
+// //Uncaught TypeError: console.lo is not a function
+// console.log("and they're always glad you came.");
+// //Uncaught syntaxerror: missing after argument list
+// console.log("You wanna be where you can see,");
+// console.log("our troubles are all the same");
+// //Uncaught syntaxerror missing after argument list
+// console.log("You wanna be where everybody knows");
+// console.log("Your name.");
+
+//Assignment Operator vs Equality Operator
+//vs Strict Equality Operator
+
+// assignment operator =
+//equality operator ==
+//strict equality operator ===
+
+//While Loops: reps and reps
+
+// let num = 1;
+// while(num <= 10) {
+//   console.log('Ginger chocolate honey patties');
+//   num++
+// }
+
+// let num = 0;
+// while(num <= 1000) {
+//   console.log(num);
+//   num++
+// }
+
+// let num = 0;
+// while(num <= 1000) {
+//   console.log(`Current loop number is: `,num);
+//   num++
+// }
+
+// for(let i = 0; i <= 100; i++) {
+//   console.log(i);
+// }
+
+// for(let i = 7; i <=635; i++) {
+//   console.log(i);
+// }
+
+let start = 0;
+const limit = 100;
+
+for (let i = start; i <= limit; i++) {
+  console.log(i);
+}
